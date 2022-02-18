2157367786f465e17ed9391bb9ca1539a21e139c
fyan0125 <elainefang2001@gmail.com>
2022-02-18 09:15:37 +0800

    wk1proj

--- App.js
+++ App.js
@@ -3,18 +3,40 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <>
+    <View style={styles.header}>
+      <Text style={styles.headerText}>WEEK 1</Text>
+    </View>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.containerText}>110819009</Text>
+      <Text>方 雁</Text>
       <StatusBar style="auto" />
     </View>
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header: {
+    paddingTop: 30,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#696773',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText:{
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#EFF1F3',
+  },
+  container: {
+    flex: 10,
+    backgroundColor: '#EFF1F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerText:{
+    color: '#272727',
+  }
 });
